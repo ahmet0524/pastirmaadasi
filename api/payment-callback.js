9f73382cea163891cef88bdf38e5cf303d25bf9f
Ahmet <ayavuz0524@gmail.com>
2025-10-21 08:02:54 +0300

    u

--- api/payment-callback.js
+++ api/payment-callback.js
@@ -1,8 +1,17 @@
 // api/payment-callback.js
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    // iyzico POST callback'i - token'ı al ve GET'e redirect et
+    // iyzico POST callback'i - token'Ä± al ve GET'e redirect et
     const { token } = req.body;
+
+    if (token) {
+      res.redirect(302, `/odeme-sonuc?token=${token}`);
+    } else {
+      res.redirect(302, '/odeme-sonuc?error=no-token');
+    }
+  } else if (req.method === 'GET') {
+    // iyzico bazen GET ile de çağırabilir
+    const { token } = req.query;
 
     if (token) {
       res.redirect(302, `/odeme-sonuc?token=${token}`);
